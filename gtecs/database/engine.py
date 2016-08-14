6a6d778973d7fd55b1a10595402c8dfa9e6de2bd
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-08-14 17:40:33 -0300

    used contextmanagers

--- gtecs/database/engine.py
+++ gtecs/database/engine.py
@@ -1,11 +1,20 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 # TODO: build name from params
-def loadSession(echo=False):
+
+@contextmanager
+def open_session(echo=False):
     """
-    Create a DB session.
+    Create a DB session context manager.
+
+    Automatically takes care of commiting changes
+    to DB when scope closes and rolls back on exceptions.
+
+    Needless to say it also closes the session when it goes out of scope.
 
     Parameters
     ----------
@@ -15,7 +24,15 @@ def loadSession(echo=False):
     Returns
     -------
     session : `sqlalchemy.orm.session.Session`
-        a database session
+        a database session context manager
+
+    Examples
+    --------
+    To create a new user, and add to the database:
+
+    >>> with open_session() as session:
+    >>>     newUser = User('sl', '1234', 'Stuey')
+    >>>     session.add(newUser)
     """
     engine = create_engine(
         'mysql+pymysql://goto:gotoobs@localhost/goto_obs',
@@ -23,4 +40,11 @@ def loadSession(echo=False):
         )
     Session = sessionmaker(bind=engine)
     session = Session()
-    return session
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()

fc9b3eb268d237b433db7147ee907d86acb6c5b1
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-02-20 17:35:49 +0000

    Tidy tables

--- schema.sql
+++ schema.sql
@@ -21,21 +21,27 @@ USE `goto_obs` ;
 -- Events table
 DROP TABLE IF EXISTS `events` ;
 CREATE TABLE `events` (
+  -- columns
   `eventID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `source` VARCHAR(255) NOT NULL COMMENT 'LIGO, SWIFT etc.',
   `ivo` VARCHAR(255) NOT NULL UNIQUE,
   `skymap` VARCHAR(255) NULL
+  -- indexes
+  -- foreign keys
   )
   ENGINE = InnoDB;
 
 -- Users table
 DROP TABLE IF EXISTS `users` ;
 CREATE TABLE IF NOT EXISTS `users` (
+  -- columns
   `userKey` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_name` VARCHAR(255) NOT NULL UNIQUE,
   `password` VARCHAR(255) NOT NULL,
   `fullName` TEXT NOT NULL
+  -- indexes
+  -- foreign keys
   )
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = latin1;
@@ -43,18 +49,24 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Surveys table
 DROP TABLE IF EXISTS `surveys` ;
 CREATE TABLE `surveys` (
+  -- columns
   `surveyID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL
+  -- indexes
+  -- foreign keys
   )
   ENGINE = InnoDB;
 
 -- Survey tiles table
 DROP TABLE IF EXISTS `survey_tiles` ;
 CREATE TABLE `survey_tiles` (
+  -- columns
   `tileID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
   `ra` FLOAT NOT NULL COMMENT 'decimal degrees',
   `decl` FLOAT NOT NULL COMMENT 'decimal degrees',
+  -- indexes
+  -- foreign keys
   `surveys_surveyID` INT NOT NULL,
   INDEX `fk_survey_tiles_surveys1_idx` (`surveys_surveyID`),
   CONSTRAINT `fk_survey_tiles_surveys1`
@@ -66,27 +78,27 @@ CREATE TABLE `survey_tiles` (
 -- Event tiles table
 DROP TABLE IF EXISTS `event_tiles` ;
 CREATE TABLE`event_tiles` (
+  -- columns
   `tileID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `ra` FLOAT NOT NULL,
   `decl` FLOAT NOT NULL,
   `probability` FLOAT NOT NULL,
   `unobserved_probability` FLOAT NOT NULL,
+  -- indexes
+  -- foreign keys
   `events_eventID` INT NOT NULL,
   `survey_tiles_tileID` INT NULL,
   INDEX `fk_event_tiles_events1_idx` (`events_eventID`),
   INDEX `fk_event_tiles_survey_tiles1_idx` (`survey_tiles_tileID`),
-  CONSTRAINT `fk_event_tiles_events1`
-    FOREIGN KEY (`events_eventID`)
-    REFERENCES `events` (`eventID`),
-  CONSTRAINT `fk_event_tiles_survey_tiles1`
-    FOREIGN KEY (`survey_tiles_tileID`)
-    REFERENCES `survey_tiles` (`tileID`)
+  CONSTRAINT `fk_event_tiles_events1` FOREIGN KEY (`events_eventID`) REFERENCES `events` (`eventID`),
+  CONSTRAINT `fk_event_tiles_survey_tiles1` FOREIGN KEY (`survey_tiles_tileID`) REFERENCES `survey_tiles` (`tileID`)
   )
   ENGINE = InnoDB;
 
 -- Mpointings table
 DROP TABLE IF EXISTS `mpointings` ;
 CREATE TABLE `mpointings` (
+  -- columns
   `mpointingID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `status` ENUM('unscheduled', 'scheduled', 'completed', 'aborted', 'expired', 'deleted') NOT NULL DEFAULT 'unscheduled',
   `object` TEXT NOT NULL,
@@ -105,6 +117,11 @@ CREATE TABLE `mpointings` (
   `stopUTC` DATETIME NULL COMMENT 'If Null then the Mpointing will continue until it is complete',
   `num_todo` INT NOT NULL,
   `num_completed` INT NOT NULL DEFAULT 0,
+  -- indexes
+  INDEX `status_idx` (`status`),
+  INDEX `startUTC_idx` (`startUTC`),
+  INDEX `stopUTC_idx` (`stopUTC`),
+  -- foreign keys
   `users_userKey` INT NOT NULL,
   `surveys_surveyID` INT NULL,
   `survey_tiles_tileID` INT NULL,
@@ -115,48 +132,37 @@ CREATE TABLE `mpointings` (
   INDEX `fk_mpointings_survey_tiles1_idx` (`survey_tiles_tileID`),
   INDEX `fk_mpointings_event_tiles1_idx` (`event_tiles_tileID`),
   INDEX `fk_mpointings_surveys1_idx` (`surveys_surveyID`),
-  INDEX `status_idx` (`status`),
-  INDEX `startUTC_idx` (`startUTC`),
-  INDEX `stopUTC_idx` (`stopUTC`),
-  CONSTRAINT `fk_mpointing_events1`
-    FOREIGN KEY (`events_eventID`)
-    REFERENCES `events` (`eventID`),
-  CONSTRAINT `fk_mpointing_users1`
-    FOREIGN KEY (`users_userKey`)
-    REFERENCES `users` (`userKey`),
-  CONSTRAINT `fk_mpointings_survey_tiles1`
-    FOREIGN KEY (`survey_tiles_tileID`)
-    REFERENCES `survey_tiles` (`tileID`),
-  CONSTRAINT `fk_mpointings_event_tiles1`
-    FOREIGN KEY (`event_tiles_tileID`)
-    REFERENCES `event_tiles` (`tileID`),
-  CONSTRAINT `fk_mpointings_surveys1`
-    FOREIGN KEY (`surveys_surveyID`)
-    REFERENCES `surveys` (`surveyID`)
+  CONSTRAINT `fk_mpointing_events1` FOREIGN KEY (`events_eventID`) REFERENCES `events` (`eventID`),
+  CONSTRAINT `fk_mpointing_users1` FOREIGN KEY (`users_userKey`) REFERENCES `users` (`userKey`),
+  CONSTRAINT `fk_mpointings_survey_tiles1` FOREIGN KEY (`survey_tiles_tileID`) REFERENCES `survey_tiles` (`tileID`),
+  CONSTRAINT `fk_mpointings_event_tiles1` FOREIGN KEY (`event_tiles_tileID`) REFERENCES `event_tiles` (`tileID`),
+  CONSTRAINT `fk_mpointings_surveys1` FOREIGN KEY (`surveys_surveyID`) REFERENCES `surveys` (`surveyID`)
   )
   ENGINE = InnoDB;
 
 -- Observing blocks table
 DROP TABLE IF EXISTS `observing_blocks` ;
 CREATE TABLE `observing_blocks` (
+  -- columns
   `blockID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `blockNum` INT NOT NULL,
   `current` BOOLEAN NOT NULL DEFAULT FALSE,
   `valid_time` FLOAT NOT NULL COMMENT 'how long after the startUTC the pointing should be valid for in minutes',
   `wait_time` FLOAT NOT NULL COMMENT 'time to wait after this pointing before scheduling the next',
-  `mpointings_mpointingID` INT NOT NULL,
-  INDEX `fk_observing_blocks_mpointing1_idx` (`mpointings_mpointingID`),
+  -- indexes
   INDEX `blockNum_idx` (`blockNum`),
   INDEX `current_idx` (`current`),
-  CONSTRAINT `fk_observing_blocks_mpointing1`
-    FOREIGN KEY (`mpointings_mpointingID`)
-    REFERENCES `mpointings` (`mpointingID`)
+  -- foreign keys
+  `mpointings_mpointingID` INT NOT NULL,
+  INDEX `fk_observing_blocks_mpointing1_idx` (`mpointings_mpointingID`),
+  CONSTRAINT `fk_observing_blocks_mpointing1` FOREIGN KEY (`mpointings_mpointingID`) REFERENCES `mpointings` (`mpointingID`)
   )
   ENGINE = InnoDB;
 
 -- Pointings table
 DROP TABLE IF EXISTS `pointings` ;
 CREATE TABLE `pointings` (
+  -- columns
   `pointingID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `status` ENUM('pending', 'running', 'completed', 'aborted', 'interrupted', 'expired', 'deleted') NOT NULL DEFAULT 'pending',
   `object` TEXT NOT NULL COMMENT 'object name',
@@ -174,6 +180,11 @@ CREATE TABLE `pointings` (
   `startedUTC` DATETIME NULL,
   `stoppedUTC` DATETIME NULL,
   `ts` TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+  -- indexes
+  INDEX `status_idx` (`status`),
+  INDEX `startUTC_idx` (`startUTC`),
+  INDEX `stopUTC_idx` (`stopUTC`),
+  -- foreign keys
   `users_userKey` INT NOT NULL,
   `mpointings_mpointingID` INT NULL,
   `observing_blocks_blockID` INT NULL,
@@ -185,33 +196,16 @@ CREATE TABLE `pointings` (
   INDEX `fk_pointings_users1_idx` (`users_userKey`),
   INDEX `fk_pointings_observing_blocks1_idx` (`observing_blocks_blockID`),
   INDEX `fk_pointings_mpointings1_idx` (`mpointings_mpointingID`),
-  INDEX `status_idx` (`status`),
-  INDEX `startUTC_idx` (`startUTC`),
-  INDEX `stopUTC_idx` (`stopUTC`),
   INDEX `fk_pointings_event_tiles1_idx` (`event_tiles_tileID`),
   INDEX `fk_pointings_survey_tiles1_idx` (`survey_tiles_tileID`),
   INDEX `fk_pointings_surveys1_idx` (`surveys_surveyID`),
-  CONSTRAINT `fk_pointings_events1`
-    FOREIGN KEY (`events_eventID`)
-    REFERENCES `events` (`eventID`),
-  CONSTRAINT `fk_pointings_users1`
-    FOREIGN KEY (`users_userKey`)
-    REFERENCES `users` (`userKey`),
-  CONSTRAINT `fk_pointings_observing_blocks1`
-    FOREIGN KEY (`observing_blocks_blockID`)
-    REFERENCES `observing_blocks` (`blockID`),
-  CONSTRAINT `fk_pointings_mpointings1`
-    FOREIGN KEY (`mpointings_mpointingID`)
-    REFERENCES `mpointings` (`mpointingID`),
-  CONSTRAINT `fk_pointings_event_tiles1`
-    FOREIGN KEY (`event_tiles_tileID`)
-    REFERENCES `event_tiles` (`tileID`),
-  CONSTRAINT `fk_pointings_survey_tiles1`
-    FOREIGN KEY (`survey_tiles_tileID`)
-    REFERENCES `survey_tiles` (`tileID`),
-  CONSTRAINT `fk_pointings_surveys1`
-    FOREIGN KEY (`surveys_surveyID`)
-    REFERENCES `surveys` (`surveyID`)
+  CONSTRAINT `fk_pointings_users1` FOREIGN KEY (`users_userKey`) REFERENCES `users` (`userKey`),
+  CONSTRAINT `fk_pointings_mpointings1` FOREIGN KEY (`mpointings_mpointingID`) REFERENCES `mpointings` (`mpointingID`),
+  CONSTRAINT `fk_pointings_events1` FOREIGN KEY (`events_eventID`) REFERENCES `events` (`eventID`),
+  CONSTRAINT `fk_pointings_observing_blocks1` FOREIGN KEY (`observing_blocks_blockID`) REFERENCES `observing_blocks` (`blockID`),
+  CONSTRAINT `fk_pointings_event_tiles1` FOREIGN KEY (`event_tiles_tileID`) REFERENCES `event_tiles` (`tileID`),
+  CONSTRAINT `fk_pointings_survey_tiles1` FOREIGN KEY (`survey_tiles_tileID`) REFERENCES `survey_tiles` (`tileID`),
+  CONSTRAINT `fk_pointings_surveys1` FOREIGN KEY (`surveys_surveyID`) REFERENCES `surveys` (`surveyID`)
   )
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = latin1;
@@ -219,6 +213,7 @@ CREATE TABLE `pointings` (
 -- Exposure sets table
 DROP TABLE IF EXISTS `exposure_sets` ;
 CREATE TABLE `exposure_sets` (
+  -- columns
   `expID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `utMask` INT NULL COMMENT 'bit mask to allocate to individual UTs. NULL means send to all',
   `typeFlag` ENUM('SCIENCE', 'FOCUS', 'DARK', 'BIAS', 'FLAT', 'STD') NOT NULL,
@@ -228,16 +223,14 @@ CREATE TABLE `exposure_sets` (
   `numexp` INT UNSIGNED NOT NULL,
   `raoff` FLOAT NOT NULL DEFAULT 0.0 COMMENT 'arcsecs',
   `decoff` FLOAT NOT NULL DEFAULT 0.0 COMMENT 'arcsec',
+  -- indexes
+  -- foreign keys
   `pointings_pointingID` INT NULL,
   `mpointings_mpointingID` INT NULL,
   INDEX `fk_exposures_pointings1_idx` (`pointings_pointingID`),
   INDEX `fk_exposures_mpointing1_idx` (`mpointings_mpointingID`),
-  CONSTRAINT `fk_exposures_pointings1`
-    FOREIGN KEY (`pointings_pointingID`)
-    REFERENCES `pointings` (`pointingID`),
-  CONSTRAINT `fk_exposures_mpointing1`
-    FOREIGN KEY (`mpointings_mpointingID`)
-    REFERENCES `mpointings` (`mpointingID`)
+  CONSTRAINT `fk_exposures_pointings1` FOREIGN KEY (`pointings_pointingID`) REFERENCES `pointings` (`pointingID`),
+  CONSTRAINT `fk_exposures_mpointing1` FOREIGN KEY (`mpointings_mpointingID`) REFERENCES `mpointings` (`mpointingID`)
   )
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = latin1;
@@ -245,6 +238,7 @@ CREATE TABLE `exposure_sets` (
 -- Image logs table
 DROP TABLE IF EXISTS `image_logs` ;
 CREATE TABLE `image_logs` (
+  -- columns
   `logID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `filename` VARCHAR(30) NOT NULL UNIQUE COMMENT 'full FITS file name, including extension',
   `runNumber` INT NOT NULL,
@@ -254,23 +248,19 @@ CREATE TABLE `image_logs` (
   `writeUTC` DATETIME NOT NULL,
   `set_position` INT NOT NULL DEFAULT 1,
   `set_total` INT NOT NULL DEFAULT 1,
+  -- indexes
+  INDEX `runNumber_idx` (`runNumber`),
+  INDEX `writeUTC_idx` (`writeUTC`),
+  -- foreign keys
   `exposure_sets_expID` INT NULL DEFAULT NULL,
   `pointings_pointingID` INT NULL DEFAULT NULL,
   `mpointings_mpointingID` INT NULL DEFAULT NULL,
   INDEX `fk_image_logs_exposure_sets1_idx` (`exposure_sets_expID`),
   INDEX `fk_image_logs_pointings1_idx` (`pointings_pointingID`),
   INDEX `fk_image_logs_mpointings1_idx` (`mpointings_mpointingID`),
-  INDEX `runNumber_idx` (`runNumber`),
-  INDEX `writeUTC_idx` (`writeUTC`),
-  CONSTRAINT `fk_image_logs_exposure_sets1`
-    FOREIGN KEY (`exposure_sets_expID`)
-    REFERENCES `exposure_sets` (`expID`),
-  CONSTRAINT `fk_image_logs_pointings1`
-    FOREIGN KEY (`pointings_pointingID`)
-    REFERENCES `pointings` (`pointingID`),
-  CONSTRAINT `fk_image_logs_mpointings1`
-    FOREIGN KEY (`mpointings_mpointingID`)
-    REFERENCES `mpointings` (`mpointingID`)
+  CONSTRAINT `fk_image_logs_exposure_sets1` FOREIGN KEY (`exposure_sets_expID`) REFERENCES `exposure_sets` (`expID`),
+  CONSTRAINT `fk_image_logs_pointings1` FOREIGN KEY (`pointings_pointingID`) REFERENCES `pointings` (`pointingID`),
+  CONSTRAINT `fk_image_logs_mpointings1` FOREIGN KEY (`mpointings_mpointingID`) REFERENCES `mpointings` (`mpointingID`)
   )
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = latin1;

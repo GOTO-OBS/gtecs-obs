462077ad0ea7b08ca85ccad67ad3ddbaa5e0a5cc
Martin Dyer <martin.dyer@sheffield.ac.uk>
2019-02-27 13:44:27 +0000

    Simplify constraints

--- gtecs/scheduler.py
+++ gtecs/scheduler.py
@@ -203,82 +203,9 @@ class PointingQueue(object):
         self.maxmoon_arr = np.array([float(MOON_PHASES[p.maxmoon]) for p in self.pointings])
         self.minmoonsep_arr = np.array([float(p.minmoonsep) for p in self.pointings])
 
-        # Create normal constraints
-        # Apply to every pointing
-        self.targets = coord.SkyCoord(self.ra_arr, self.dec_arr,
-                                      unit=u.deg, frame='icrs')
+        # Create targets
+        self.targets = coord.SkyCoord(self.ra_arr, self.dec_arr, unit=u.deg, frame='icrs')
         self.mintimes = u.Quantity(self.mintime_arr, unit=u.s)
-        self.maxsunalts = u.Quantity(self.maxsunalt_arr, unit=u.deg)
-        self.minalts = u.Quantity(self.minalt_arr, unit=u.deg)
-        self.minmoonseps = u.Quantity(self.minmoonsep_arr, unit=u.deg)
-
-        self.constraints = [AtNightConstraint(self.maxsunalts),
-                            AltitudeConstraint(self.minalts, None),
-                            ArtificialHorizonConstraint(),
-                            MoonIlluminationConstraint(None, self.maxmoon_arr),
-                            MoonSeparationConstraint(self.minmoonseps, None)]
-        self.constraint_names = ['SunAlt',
-                                 'MinAlt',
-                                 'ArtHoriz',
-                                 'Moon',
-                                 'MoonSep']
-
-        # Create time constraints
-        # Apply to every pointing EXCEPT the current pointing
-        self.time_mask = np.array([not p.current for p in self.pointings])
-
-        if np.all(self.time_mask):
-            # no current pointing, don't bother making a new SkyCoord
-            self.targets_t = self.targets
-        else:
-            self.targets_t = coord.SkyCoord(self.ra_arr[self.time_mask],
-                                            self.dec_arr[self.time_mask],
-                                            unit=u.deg, frame='icrs')
-
-        self.starts_t = Time(self.start_arr[self.time_mask],
-                             scale='utc', format='datetime')
-        # The stop time can be None for non-expiring pointings,
-        # but the constraint needs a Time so just say a long time from now.
-        longtime = Time.now() + 10 * u.year
-        self.stops_t = Time([stoptime if stoptime else longtime.datetime
-                            for stoptime in self.stop_arr[self.time_mask]],
-                            scale='utc', format='datetime')
-
-        self.time_constraint = [TimeConstraint(self.starts_t, self.stops_t)]
-        self.time_constraint_name = ['Time']
-
-        # Create mintime constraints
-        # Apply to all non-current pointings
-        self.mintime_mask = np.array([not p.current for p in self.pointings])
-
-        if np.all(self.mintime_mask):
-            # no current pointing => use existing objects
-            self.targets_m = self.targets
-            self.mintimes_m = self.mintimes
-            self.maxsunalts_m = self.maxsunalts
-            self.minalts_m = self.minalts
-        else:
-            self.targets_m = coord.SkyCoord(self.ra_arr[self.mintime_mask],
-                                            self.dec_arr[self.mintime_mask],
-                                            unit=u.deg, frame='icrs')
-            self.mintimes_m = u.Quantity(self.mintime_arr[self.mintime_mask],
-                                         unit=u.s)
-            self.maxsunalts_m = u.Quantity(self.maxsunalt_arr[self.mintime_mask],
-                                           unit=u.deg)
-            self.minalts_m = u.Quantity(self.minalt_arr[self.mintime_mask],
-                                        unit=u.deg)
-
-        self.mintime_constraints = [AtNightConstraint(self.maxsunalts_m),
-                                    AltitudeConstraint(self.minalts_m, None),
-                                    ArtificialHorizonConstraint()]
-        self.mintime_constraint_names = ['SunAlt_mintime',
-                                         'MinAlt_mintime',
-                                         'ArtHoriz_mintime']
-
-        # Save all names
-        self.all_constraint_names = (self.constraint_names +
-                                     self.time_constraint_name +
-                                     self.mintime_constraint_names)
 
     def __len__(self):
         return len(self.pointings)
@@ -326,49 +253,70 @@ class PointingQueue(object):
 
     def check_validities(self, now, observer):
         """Check if the pointings are valid, both now and after mintimes."""
-        # apply normal constraints
-        cons_valid_arr = apply_constraints(self.constraints,
-                                           observer,
-                                           self.targets,
-                                           now)
+        # Create Constraints
+        self.constraints = {}
 
-        # apply time constraint to filtered targets
-        if np.any(self.time_mask):
-            time_cons_valid_arr = apply_constraints(self.time_constraint,
-                                                    observer,
-                                                    self.targets_t,
-                                                    now)
-        else:
-            time_cons_valid_arr = np.array([])
+        # SunAlt
+        self.maxsunalts = u.Quantity(self.maxsunalt_arr, unit=u.deg)
+        self.constraints['SunAlt'] = AtNightConstraint(self.maxsunalts)
 
-        # apply mintime constraints to filtered targets
-        if any(self.mintime_mask):
-            later_arr = now + self.mintimes_m
-            min_cons_valid_arr = apply_constraints(self.mintime_constraints,
-                                                   observer,
-                                                   self.targets_m,
-                                                   later_arr)
-        else:
-            min_cons_valid_arr = np.array([])
+        # MinAlt
+        self.minalts = u.Quantity(self.minalt_arr, unit=u.deg)
+        self.constraints['MinAlt'] = AltitudeConstraint(self.minalts, None)
 
-        # save the results on the pointings
+        # ArtHoriz
+        self.constraints['ArtHoriz'] = ArtificialHorizonConstraint()
+
+        # Moon
+        self.constraints['Moon'] = MoonIlluminationConstraint(None, self.maxmoon_arr)
+
+        # MoonSep
+        self.minmoonseps = u.Quantity(self.minmoonsep_arr, unit=u.deg)
+        self.constraints['MoonSep'] = MoonSeparationConstraint(self.minmoonseps, None)
+
+        # Time
+        self.starts_t = Time(self.start_arr,
+                             scale='utc', format='datetime')
+        # NB the stop time can be None for non-expiring pointings,
+        #    but the constraint needs a Time so just say a long time from now.
+        longtime = Time.now() + 10 * u.year
+        self.stops_t = Time([stoptime if stoptime else longtime.datetime
+                            for stoptime in self.stop_arr],
+                            scale='utc', format='datetime')
+        self.constraints['Time'] = TimeConstraint(self.starts_t, self.stops_t)
+
+        # Apply constraints
+        normal_cons = ['SunAlt', 'MinAlt', 'ArtHoriz', 'Moon', 'MoonSep']
+        cons_valid_arr = apply_constraints([self.constraints[name] for name in normal_cons],
+                                           observer, self.targets, now)
+        time_cons = ['Time']
+        time_cons_valid_arr = apply_constraints([self.constraints[name] for name in time_cons],
+                                                observer, self.targets, now)
+        mintime_cons = ['SunAlt', 'MinAlt', 'ArtHoriz']
+        min_cons_valid_arr = apply_constraints([self.constraints[name] for name in mintime_cons],
+                                               observer, self.targets, now)
+        mintime_names = ['SunAlt_mintime', 'MinAlt_mintime', 'ArtHoriz_mintime']
+        self.all_constraint_names = normal_cons + time_cons + mintime_names
+
+        # Save constraint results on Pointings and calculate if they are valid
         for i, pointing in enumerate(self.pointings):
-            pointing.constraint_names = list(self.constraint_names)
+            # normal constraints
+            pointing.constraint_names = list(normal_cons)
             pointing.valid_arr = cons_valid_arr[i]
 
-        for i, pointing in enumerate(self.pointings[self.time_mask]):
-            pointing.constraint_names += self.time_constraint_name
-            pointing.valid_arr = np.concatenate((pointing.valid_arr,
-                                                 time_cons_valid_arr[i]))
+            # extra constraints
+            if not pointing.current:
+                pointing.constraint_names += time_cons
+                pointing.valid_arr = np.concatenate((pointing.valid_arr,
+                                                     time_cons_valid_arr[i]))
+                pointing.constraint_names += mintime_names
+                pointing.valid_arr = np.concatenate((pointing.valid_arr,
+                                                    min_cons_valid_arr[i]))
 
-        for i, pointing in enumerate(self.pointings[self.mintime_mask]):
-            pointing.constraint_names += self.mintime_constraint_names
-            pointing.valid_arr = np.concatenate((pointing.valid_arr,
-                                                 min_cons_valid_arr[i]))
-
-        # finally find out if each pointing is valid or not
-        for pointing in self.pointings:
+            # save all constraint names on all
             pointing.all_constraint_names = self.all_constraint_names
+
+            # finally find out if each pointing is valid or not
             pointing.valid = np.all(pointing.valid_arr)
             pointing.valid_time = now
 

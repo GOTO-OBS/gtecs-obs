14feb77ea718d3d1d0c05edbb71a650d1e079c76
Martin Dyer <martin.dyer@sheffield.ac.uk>
2023-04-12 15:41:33 +0100

    Try to import alert db if installed

--- gtecs/obs/database/__init__.py
+++ gtecs/obs/database/__init__.py
@@ -3,3 +3,10 @@
 from .models import *  # noqa: F401, F403
 from .random import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
+
+try:
+    # Try to use the alert database if it is available
+    # This will add in the back-references to Events and Notices
+    from gtecs.alert import database as alert_db
+except Exception:
+    pass
